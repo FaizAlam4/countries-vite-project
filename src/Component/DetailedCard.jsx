da56c976f40b54cc15e127c8ced46421506a00d3
FaizAlam4 <faizalam4e@gmail.com>
2024-01-03 10:58:45 +0530

    added detailed view info in cards and set paddings

--- src/Component/DetailedCard.jsx
+++ src/Component/DetailedCard.jsx
@@ -9,54 +9,60 @@ function DetailedCard(props) {
 
   return (
     <>
-    <Link to={`/`}><button >back</button></Link> 
-    <div className="card-container-item-detail">
-     
-      <div className="card-container-item-detail-info">
-        <img src={props.country.flags.png} alt="" />
-      </div>
+      <Link to={`/`}>
+        <button>back</button>
+      </Link>
+      <div className={`card-container-item-detail ${theme}-card-detail`}>
+        <div className="card-container-item-detail-info">
+          <img src={props.country.flags.png} alt="" />
+        </div>
 
-      <div className={`card-container-item-detail-info ${theme}-card-detail`}>
-        <h3 className="list-item-title">{props.country.name.common}</h3>
+        <div className={`card-container-item-detail-info ${theme}-card-detail`}>
+          <h3 className="list-item-title">{props.country.name.common}</h3>
 
-        <ul className="list-wrapper-detail">
-          <li className="list-item-detail">
-            <b className={`${theme}-card`}>Population: </b>{" "}
-            {props.country.population}
-          </li>
-          <li className="list-item-detail">
-            <b className={`${theme}-card`}>Region: </b>
-            {props.country.region}
-          </li>
-          <li className="list-item-detail">
-            <b className={`${theme}-card`}>Sub Region: </b>
-            {props.country.subregion}
-          </li>
-          <li className="list-item-detail">
-            <b className={`${theme}-card`}>Capital: </b>
-            {props.country.capital}
-          </li>
-        </ul>
-      </div>
+          <ul className="list-wrapper-detail">
+            <li className="list-item-detail">
+              <b>Native Name: </b> {props.country.name.nativeName[Object.keys(props.country.name.nativeName)[0]].official}
+            </li>
+            <li className="list-item-detail">
+              <b>Population: </b> {props.country.population.toLocaleString()}
+            </li>
+            <li className="list-item-detail">
+              <b>Region: </b>
+              {props.country.region}
+            </li>
+            <li className="list-item-detail">
+              <b>Sub Region: </b>
+              {props.country.subregion}
+            </li>
+            <li className="list-item-detail">
+              <b>Capital: </b>
+              {props.country.capital}
+            </li>
+          </ul>
+        </div>
 
-      <div className={`card-container-item-detail-info ${theme}-card`}>
-        <ul className="list-wrapper-detail">
-          <li className="list-item-detail">
-            <b className={`${theme}-card`}>Top Level Domain: </b>
-            {props.country.tld}
-          </li>
-          <li className="list-item-detail">
-            <b className={`${theme}-card`}>Currencies: </b>
-            {props.country.tld}
-          </li>
-          <li className="list-item-detail">
-            <b className={`${theme}-card`}>Languages: </b>
-            {Object.keys(props.country.languages).map((ele) => props.country.languages[ele] + " "
-            )}
-          </li>
-        </ul>
+        <div className={`card-container-item-detail-info ${theme}-card`}>
+          <ul className="list-wrapper-detail">
+            <li className="list-item-detail">
+              <b>Top Level Domain: </b>
+              {props.country.tld}
+            </li>
+            <li className="list-item-detail">
+              <b>Currencies: </b>
+              {Object.keys(props.country.currencies).map(
+                (ele) => props.country.currencies[ele]["name"]
+              )}
+            </li>
+            <li className="list-item-detail">
+              <b>Languages: </b>
+              {Object.keys(props.country.languages).map(
+                (ele) => props.country.languages[ele] + " "
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
     </>
   );
 }
